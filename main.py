452a2a105e782d8f611c49e7c59131c9b0ea2e36
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-18 21:52:38 +0530

    api created and data file added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import pandas as pd
 app = Flask(__name__)
 
 
@@ -8,11 +8,19 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/api/v1/<name>")
-def api(name):
-    upper_word = name.upper()
-    result_dictionary = {"name": name, "capital": upper_word}
-    return result_dictionary
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    print(station)
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
 
 
 app.run(debug=True)
