d66ffeb74dcdeee05dddd5f1829a2f7fba76467f
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-18 12:14:46 +0530

    home.html added and flask on main file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<name>")
+def api(name):
+    upper_word = name.upper()
+    result_dictionary = {"name": name, "capital": upper_word}
+    return result_dictionary
+
+
+app.run(debug=True)
